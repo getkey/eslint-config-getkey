ea3982b060ca6cc69345d42239688e4f087818f8
Julien Mourer <getkey@getkey.eu>
2018-09-02 14:17:23 +0200

    Add eslint as a peerDependency, disallow trailing whitespace

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+/* eslint-env node */
+'use strict';
+
 const fs = require('fs');
 const path = require('path');
 const yaml = require('js-yaml');
 
-module.exports = yaml.load(
-	fs.readFileSync(path.join(__dirname, '.eslintrc.yml')),
-);
+const eslintrc = fs.readFileSync(path.join(__dirname, '.eslintrc.yml'));
+module.exports = yaml.load(eslintrc);
